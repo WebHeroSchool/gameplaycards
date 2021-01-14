ec06c09aa18d3fa025e820fd995f411241a52ddd
ElenaSindi <sindielena@icloud.com>
2021-01-14 16:47:44 +0300

    изменила содержимое index.html, style.css, three.html

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+let simple = document.getElementById('simple');
+let middle = document.getElementById('middle');
+let hard = document.getElementById('hard');
+let choice = document.querySelectorAll('.selection__level');
+let btn = document.querySelector('.selection__btn');
+let cards = document.querySelectorAll('.game__cards');
+let six = document.querySelectorAll('.cardsSix');
+let three = document.querySelectorAll('.cardsThree');
+let haveFlippedCard = false;
+let	yourCard;
+let level;
+let picture1 = document.querySelector('.selection__rectangular');
+let picture2 = document.querySelector('.selection__rectangul');
+let picture3 = document.querySelector('.selection__rectan');
+
 // выбор сложности
 
-let simple = document.querySelector('.selection__simple');
-let middle = document.querySelector('.selection__middle');
-let hard = document.querySelector('.selection__hard');
-let choice;
-let btn = document.querySelectorAll('.selection__btn');
-/*
 function yourChoice() {
-	this.classList.toggle('selection__rectangle');
+	if (!level) {
+	this.classList.add('active');
 	choice = this;
+	level = true;
+	//picture.forEach(elm => elm.classList.remove('hidden'));
+	}
   } 
-simple.addEventListener('click', yourChoice());
-middle.addEventListener('click', yourChoice());
-hard.addEventListener('click', yourChoice());
-*/
+choice.forEach (elm => elm.addEventListener("click", yourChoice));
+
+if (choice == 'simple') {
+	picture1.classList.remove('hidden');
+	three.forEach(elm => elm.classList.toggle('hidden'));	
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 3);
+	});		
+} else if (choice == 'middle') {
+	picture2.classList.remove('hidden');	
+	six.forEach(elm => elm.classList.toggle('hidden'));
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 6);
+	});
+} else if (choice == 'hard') {
+	picture3.classList.remove('hidden');
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 9);
+	});	
+}
+
+btn.onclick = function() {window.location.href='three.html';}
 
 // нажатие на кнопку
 /*
-btn.onclick = function game() {
-	if (choice = simple) {
-			window.location.href = 'three.html';
-	} else if (choice = middle) {
-			window.location.href = 'six.html';
-	} else if (choice = hard) {	
-			window.location.href = 'nine.html';
+btn.onclick = function () {
+	if (choice == simple) {
+		window.location.href='three.html';
+		three.forEach(elm => elm.classList.toggle('hidden'));	
+			//change();
+	} else if (choice == middle) {
+		window.location.href='three.html';
+		 six.forEach(elm => elm.classList.toggle('hidden'));
+			//change();
+	} else {
+		window.location.href='three.html';
+		//change();
 	}
-} 
-
-btn.onclick = function game(){window.location.href = 'three.html'}
+};
 */
-btn.onclick = function(){window.location.href='three.html'};
-// <button class="selection__btn" onclick="window.location.href='three.html'">Начать игру</button> - так работает
-
-// смена сторон карты
-
-let card = document.getElementById('bug');
-let three = document.querySelectorAll('.three__cards');
-let six = document.querySelectorAll('.six__cards');
-let nine = document.querySelectorAll('.nine__cards');
-let haveFlippedCard = false;
-let	yourCard;
+// смена сторон карты  и возврат на главную страницу
 
 function flipCard() {
 	this.classList.toggle('change-sides');
@@ -53,23 +77,33 @@ function flipCard() {
 function checkCard() {
 	if (haveFlippedCard = true) {
 		yourCard.onclick = function () {	
-			window.location.href = 'index.html';
+			window.location.href ='index.html';
 		}
 	}
 }
 
-three.forEach(elem => elem.addEventListener("click", flipCard));
-six.forEach(elem => elem.addEventListener("click", flipCard));
-nine.forEach(elem => elem.addEventListener("click", flipCard));
+cards.forEach(elem => elem.addEventListener("click", flipCard));
 
 // перемешивание карт
 /*
 function change() {
-	three.forEach(elem => {
-		let randomCard = Math.floor(Math.random() * 3);
+	if (choice == simple) {
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 3);
 	});
+	}
+	if (choice == middle) {
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 6);
+	});
+	}
+	if (choice == hard) {
+	cards.forEach(elem => {
+		var randomCard = Math.floor(Math.random() * 9);
+	});
+	}
 }
-
 */
+
 
 
